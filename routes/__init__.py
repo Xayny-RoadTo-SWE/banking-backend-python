1cfa14f82506b68ca1e31c0b313a436bda369914
Alexsandra <46141302+Alexsandra127@users.noreply.github.com>
2026-01-25 16:09:21 -0300

    Remove comments from routes __init__.py

--- routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Pacote responsável por agrupar todas as rotas (endpoints HTTP) da aplicação.
-
-Cada arquivo dentro de routes representa um domínio do sistema bancário
-(customers, users, accounts, transactions).
-"""
